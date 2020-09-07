8862aa99f296e1eea4e40d40f172ae13030ac7df
jayaprakashbl <jayaprakash.bl@genpact.com>
2020-09-07 22:23:45 +0530

    added code

--- app.py
+++ app.py
@@ -16,6 +16,21 @@ conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                       'Trusted_Connection=no;')
 cursor = conn.cursor()  
 
+@app.route('/dqservices/metadata/v1/all/',methods=['GET'], endpoint='getallmetadata')
+def getallmetadata(): 
+ storedProc = "DQS.[USP_GetColumnMetadata]"
+ cursor.execute(storedProc)
+ recs=cursor.fetchall()
+ itemsdata = []
+ for row in recs:
+        row0= (str(row[0]))
+        itemsdata.append({'servername' :row[0], 'sourcetype' :( row[1]), 'databasename' :( row[2]), 'tablename' :( row[3]), 'columnmetadataid' :( row[4]), 'sourcetableid' :( row[5]), 'columnname' :( row[6]), 'columnordinalposition' :( row[7]), 'defaultvalue' :( row[8]), 'isnullable' :( row[9]), 'datatype' :( row[10]), 'stringlength' :( row[11]), 'numericlength' :( row[12]), 'isprimary' :( row[13]), 'isalphanumeric' :( row[14]), 'regex' :( row[15]), 'issensitivecolumn' :( row[16]), 'ismandatory' :( row[17]), 'ignorevalidation' :( row[18])})
+ itemsobj=({"metadata":itemsdata})
+ data= json.dumps(itemsobj)
+ return(data)
+
+
+
 @app.route('/dqservices/metadata/v1/<id>',methods=['GET'], endpoint='getmetadata')
 def getmetadata(id): 
  storedProc = "DQS.USP_GetColumnMetadataBasedOnRowID "+id
