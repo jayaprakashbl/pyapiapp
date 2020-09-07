e353dbe5556fedc4f21cf34d23f4005979a97fad
jayaprakashbl <jayaprakash.bl@genpact.com>
2020-09-07 21:19:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,31 @@ conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                       'Trusted_Connection=no;')
 cursor = conn.cursor()  
 
-@app.route('/dqservices/metadata/v1/<id>',methods=['GET'])
-def getmetadata(id): 
- storedProc = "DQS.USP_GetColumnMetadataBasedOnRowID "+id
-
+@app.route('/dqservices/metadata/v1/',methods=['GET'])
+def getmetadata(): 
+ storedProc = "DQS.[DQS].[USP_GetColumnMetadata]"
  cursor.execute(storedProc)
-
  recs=cursor.fetchall()
- print(recs)
-
  itemsdata = []
  for row in recs:
         row0= (str(row[0]))
         itemsdata.append({'SERVERNAME' :row[0], 'SOURCETYPE' :( row[1]), 'DATABASENAME' :( row[2]), 'TABLENAME' :( row[3]), 'COLUMNMETADATAID' :( row[4]), 'SOURCETABLEID' :( row[5]), 'COLUMNNAME' :( row[6]), 'COLUMNORDINALPOSITION' :( row[7]), 'DEFAULTVALUE' :( row[8]), 'ISNULLABLE' :( row[9]), 'DATATYPE' :( row[10]), 'STRINGLENGTH' :( row[11]), 'NUMERICLENGTH' :( row[12]), 'ISPRIMARY' :( row[13]), 'ISALPHANUMERIC' :( row[14]), 'REGEX' :( row[15]), 'ISSENSITIVECOLUMN' :( row[16]), 'ISMANDATORY' :( row[17]), 'IGNOREVALIDATION' :( row[18])})
+ itemsobj=({"metadata":itemsdata})
+ data= json.dumps(itemsobj)
+ return(data)
 
+
+
+
+@app.route('/dqservices/metadata/v1/<id>',methods=['GET'])
+def getmetadata(id): 
+ storedProc = "DQS.USP_GetColumnMetadataBasedOnRowID "+id
+ cursor.execute(storedProc)
+ recs=cursor.fetchall()
+ itemsdata = []
+ for row in recs:
+        row0= (str(row[0]))
+        itemsdata.append({'SERVERNAME' :row[0], 'SOURCETYPE' :( row[1]), 'DATABASENAME' :( row[2]), 'TABLENAME' :( row[3]), 'COLUMNMETADATAID' :( row[4]), 'SOURCETABLEID' :( row[5]), 'COLUMNNAME' :( row[6]), 'COLUMNORDINALPOSITION' :( row[7]), 'DEFAULTVALUE' :( row[8]), 'ISNULLABLE' :( row[9]), 'DATATYPE' :( row[10]), 'STRINGLENGTH' :( row[11]), 'NUMERICLENGTH' :( row[12]), 'ISPRIMARY' :( row[13]), 'ISALPHANUMERIC' :( row[14]), 'REGEX' :( row[15]), 'ISSENSITIVECOLUMN' :( row[16]), 'ISMANDATORY' :( row[17]), 'IGNOREVALIDATION' :( row[18])})
  itemsobj=({"metadata":itemsdata})
  data= json.dumps(itemsobj)
  return(data)
