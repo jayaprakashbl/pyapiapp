a0b4903a4432099e716b506879fe0e4adb7f3679
jayaprakashbl <jayaprakash.bl@genpact.com>
2020-09-17 20:37:59 +0530

    Code Review

--- app.py
+++ app.py
@@ -16,21 +16,6 @@ conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                       'Trusted_Connection=no;')
 cursor = conn.cursor()  
 
-#Get all metadata
-@app.route('/dqservices/metadata/v1/all',methods=['GET'], endpoint='getallmetadata')
-def getallmetadata(): 
- storedProc = "DQS.[USP_GetColumnMetadata]"
- cursor.execute(storedProc)
- recs=cursor.fetchall()
- itemsdata = []
- for row in recs:
-        row0= (str(row[0]))
-        itemsdata.append({'servername' :row[0], 'sourcetype' :( row[1]), 'databasename' :( row[2]), 'tablename' :( row[3]), 'columnmetadataid' :( row[4]), 'sourcetableid' :( row[5]), 'columnname' :( row[6]), 'columnordinalposition' :( row[7]), 'defaultvalue' :( row[8]), 'isnullable' :( row[9]), 'datatype' :( row[10]), 'stringlength' :( row[11]), 'numericlength' :( row[12]), 'isprimary' :( row[13]), 'isalphanumeric' :( row[14]), 'regex' :( row[15]), 'issensitivecolumn' :( row[16]), 'ismandatory' :( row[17]), 'ignorevalidation' :( row[18])})
- itemsobj=({"output":itemsdata})
- data= json.dumps(itemsobj)
- return(data)
-
-
 #Get metadata based on id
 @app.route('/dqservices/metadata/v1/<id>',methods=['GET'], endpoint='getmetadata')
 def getmetadata(id): 
@@ -48,10 +33,8 @@ def getmetadata(id):
 
 #Update metadata
 @app.route('/dqservices/metadata/v1',methods=['POST'], endpoint='updatemetadata')
-
 def updatemetadata():
  metadata = request.get_json("force = True")
- 
  columnmetadataid = metadata['columnmetadataid']
  isprimary = metadata['isprimary']
  isalphanumeric = metadata['isalphanumeric']
@@ -59,17 +42,11 @@ def updatemetadata():
  issensitivecolumn = metadata['issensitivecolumn']
  ismandatory = metadata['ismandatory']
  ignorevalidation = metadata['ignorevalidation']
- 
-  
  sql = " EXEC DQS.USP_UpdateColumnMetadataBasedOnRowID @columnmetadataid=?, @isprimary=?, @isalphanumeric=?, @regex=?, @issensitivecolumn=?, @ismandatory=?, @ignorevalidation=?"
-
-
  params = (columnmetadataid,isprimary, isalphanumeric,regex,issensitivecolumn,ismandatory,ignorevalidation)
  cursor.execute(sql, params)
  cursor.commit()
- 
  response = jsonify({"message":"updated the metadata successfully"})
-
  return response
 
 
